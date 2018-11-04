f4abfe88abea7a18a94d2d27a78623e4f5a7d5a4
Mike Shoup <mike@shoup.io>
2018-11-03 23:46:36 -0600

    Remove unneeded test stuff

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,10 +8,6 @@ from werkzeug.security import generate_password_hash
 from flaskr import create_app
 from flaskr.db import init_db, db, User, Post
 
-with open(os.path.join(os.path.dirname(__file__), 'data.sql'), 'rb') as f:
-    _data_sql = f.read().decode('utf8')
-
-
 @pytest.fixture
 def app():
     db_fd, db_path = tempfile.mkstemp()
